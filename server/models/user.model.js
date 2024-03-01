45650476862314f31caba6957d63cc9226977347
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-01 21:25:17 +0800

    changes

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcryptjs from "bcryptjs"; // Fixing the typo here
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 
