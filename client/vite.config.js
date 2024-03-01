863f1de001a09ab8db0a0aff5f7c1e49a0717c9b
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-01 21:36:13 +0800

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,16 +6,6 @@ dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.REACT_APP_BACKEND_URL,
-        secure: false,
-        ws: true,
-        changeOrigin: true // Add this line to handle CORS properly
-      },
-    },
-  },
   define: {
     'process.env': process.env
   }
